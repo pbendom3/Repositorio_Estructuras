086bff104dc9e2a69c6f51f7e05be6eded854a13
A600\Patricia <p.benaventedomenec@edu.gva.es>
2025-03-11 17:04:11 +0100

    ++

--- estructuras/src/main/java/org/example/Main.java
+++ estructuras/src/main/java/org/example/Main.java
@@ -1,11 +1,34 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Stack;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
 
-        
+        Stack<String> pila = new Stack<>();
+
+        pila.push("tarta de queso");
+        pila.push("cafe");
+        pila.push("trifásico");
+
+        System.out.println(pila.peek());
+
+        System.out.println();
+        for (String postre : pila){
+            System.out.println(postre);
+        }
+
+        System.out.println();
+        while(!pila.isEmpty()){
+            System.out.println(pila.pop());
+            
+        }
+
     }
 }
